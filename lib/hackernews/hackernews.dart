e2634817d09a201e470591dacf731861d7cd8b0a
Yogesh Bhawsar <yogesh_bhawsar@outlook.com>
2019-04-07 13:21:59 +0530

    added HN item model.

--- lib/hackernews/hackernews.dart
+++ lib/hackernews/hackernews.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:toto/hackernews/models/item.dart';
+import 'package:dio/dio.dart';
 
 class HackerNews extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return ItemsList();
+  }
+}
+
+class ItemsList extends StatefulWidget {
+  @override
+  _ItemsListState createState() => _ItemsListState();
+}
+
+class _ItemsListState extends State<ItemsList> {
+  List<Item> items;
+
+  @override
+  void initState() {
+    _fetchItems();
+    super.initState();
+  }
+
+  _fetchItems() {
+    Dio().get('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then((response) {
+        print(response.data);
+      })
+      .catchError((error) {
+        print(error.response.data);
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('Hacker News'),
-      ),
+
     );
   }
 }
---
+++ lib/hackernews/models/item.dart
@@ -0,0 +1,25 @@
+import 'package:timeago/timeago.dart' as timeago;
+
+class Item {
+  final int id;
+  final String title;
+  final String by;
+  final String url;
+  final int score;
+  final String time;
+
+  Item({this.id, this.title, this.by, this.url, this.score, this.time});
+
+  factory Item.fromJson(Map json) {
+    var datetime = DateTime.fromMillisecondsSinceEpoch(json['time'] * 1000);
+
+    return Item(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      by: json['by'] as String,
+      url: json['url'] as String,
+      score: json['score'] as int,
+      time: timeago.format(datetime),
+    );
+  }
+}
